52fa85e8dc7c6ad8c3da4e1f928de7eed0deb9f7
Chema Muñoz <chemunoz78@gmail.com>
2024-06-19 17:16:44 +0200

    feat: Add task component, tasks data and filter tasks by user

---
+++ src/app/components/task/task.component.ts
@@ -0,0 +1,15 @@
+import { Component, input } from '@angular/core';
+import { Task } from '../../interfaces/task.interface';
+
+@Component({
+  selector: 'app-task',
+  standalone: true,
+  imports: [],
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.css',
+})
+export class TaskComponent {
+  task = input.required<Task>();
+
+  deleteTask(id: string) {}
+}
--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,12 +1,26 @@
 import { Component, input } from '@angular/core';
+import { TaskComponent } from '../task/task.component';
+import { Task } from '../../interfaces/task.interface';
+import { DUMMY_TASKS } from '../../dummy-tasks';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
-  name = input<string>();
+  userId = input.required<string>();
+  name = input.required<string>();
+
+  tasks: Task[] = DUMMY_TASKS;
+
+  get selectedUserTasks(): Task[] {
+    return this.tasks.filter((task) => task.userId === this.userId());
+  }
+
+  onAddTask() {}
+  onClearTasks() {}
+  onDeleteTask(id: Event) {}
 }
