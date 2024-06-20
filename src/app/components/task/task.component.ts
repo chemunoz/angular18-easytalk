77e322d3a28a94fe5e426a68ab32d2b06803032b
Chema Muñoz <chemunoz78@gmail.com>
2024-06-20 09:25:22 +0200

    feat: Add 'complete' functionality to tasks

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { Task } from '../../interfaces/task.interface';
 
 @Component({
@@ -10,6 +10,9 @@ import { Task } from '../../interfaces/task.interface';
 })
 export class TaskComponent {
   task = input.required<Task>();
+  complete = output<string>();
 
-  deleteTask(id: string) {}
+  onCompleteTask(): void {
+    this.complete.emit(this.task().id);
+  }
 }
--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -20,7 +20,10 @@ export class TasksComponent {
     return this.tasks.filter((task) => task.userId === this.userId());
   }
 
-  onAddTask() {}
-  onClearTasks() {}
-  onDeleteTask(id: Event) {}
+  onAddTask(): void {}
+
+  onCompleteTask(id: string): void {
+    // We "complete" a task by removing it from the list of tasks
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
 }
