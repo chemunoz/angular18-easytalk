d481cb1ee4b3e1f18126d8f7a1eac3dd2732d4dc
Chema Muñoz <chemunoz78@gmail.com>
2024-06-21 16:02:39 +0200

    feat: Format date data with Angular date pipe

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,11 +1,12 @@
 import { Component, input, output } from '@angular/core';
 import { CardComponent } from '@shared/card/card.component';
 import { Task } from '@interfaces/task.interface';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, DatePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
 })
